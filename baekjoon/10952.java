8a8b5b3b0ebdb4f1583baaaef42989c25440d2c5
Steve Kang <iam@juneyoung.io>
2021-05-20 22:04:52 +0900

    Solved at May 20

---
+++ baekjoon/10952.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int castAscii = (int) sc.nextLine().charAt(0);
+        System.out.println(castAscii);
+    }
+}
