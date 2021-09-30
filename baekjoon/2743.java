eeeda5c96271a174c95f890a074a9a8f5b21e111
Steve Kang <iam@juneyoung.io>
2021-09-30 09:53:13 +0900

    Solve baekjoon 2743

---
+++ baekjoon/2743.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    String text = sc.next();
+
+    System.out.println(text.length());
+  }
+}
